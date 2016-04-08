05a34c6b69f034323c7658263d78e58bed97272f
dlatushkin <dxwizard@gmail.com>
2016-04-08 22:26:53 +0300

    + RequestCountPerPeriodProcessor

---
+++ Source/Projects/Dxw.Throttling.Core/EventProcessor/RequestCountPerPeriodProcessor.cs
@@ -0,0 +1,67 @@
+﻿namespace Dxw.Throttling.Core.EventProcessor
+{
+    using System;
+    using Keyer;
+    using Rules;
+    using Storage;
+
+    public class RequestCountPerPeriodProcessor : IEventProcessor
+    {
+        private class SlotData
+        {
+            public int Hits;
+            public DateTime ExpiresAt;
+        }
+
+        private class StorageValue: IStorageValue
+        {
+            public SlotData SlotData;
+
+            public object Value => SlotData;
+
+            public bool IsExpired(DateTime utcNow)
+            {
+                return utcNow > SlotData.ExpiresAt;
+            }
+        }
+
+        public int Count { get; set; }
+        public TimeSpan Period { get; set; }
+
+        public IProcessEventResult Process(IRequestContext context = null, IStorageValue prevState = null)
+        {
+            var utcNow = DateTime.UtcNow;
+
+            ProcessEventResult result;
+            
+            StorageValue storageValue = prevState as StorageValue;
+            if (storageValue == null)
+            {
+                storageValue = new StorageValue { SlotData = new SlotData { Hits = 1, ExpiresAt = utcNow } };
+            }
+            else
+            {
+                storageValue.SlotData.Hits++;
+            }
+
+            if (storageValue.SlotData.Hits > Count)
+            {
+                result = new ProcessEventResult
+                {
+                    NewState = storageValue,
+                    Result = ApplyResult.Error("Limit of " + Count + "is exceeded. Try ")
+                };
+            }
+            else
+            {
+                result = new ProcessEventResult
+                {
+                    NewState = storageValue,
+                    Result = ApplyResult.Ok()
+                };
+            }
+            
+            return result;
+        }
+    }
+}
