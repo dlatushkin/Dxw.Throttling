e344215838c906eaf0f59d5eb4c064930278d59e
dlatushkin <dxwizard@gmail.com>
2016-04-24 11:47:07 +0300

    before IRequestContext -> object arg

---
+++ Source/Dxw.Throttling.UnitTests/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Dxw.Throttling.Core.Storage;
+using Dxw.Throttling.Core.Keyer;
+using Dxw.Throttling.Core.EventProcessor;
+using Dxw.Throttling.Core.Rules;
+
+namespace Dxw.Throttling.UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Test1()
+        {
+            var storage = new LocalMemoryStorage();
+            var keyer = new IPKeyer();
+            var processor = new ConstantEventProcessor();
+
+            var rule = new StorageKeyerProcessorRule { Storage = storage, Keyer = keyer, Processor = processor };
+
+            //var httpRequest = new HttpRequestMessage();
+            //var context = new RequestContext(RequestPhase.Before, null, )
+
+            //rule.Apply()
+        }
+    }
+}
