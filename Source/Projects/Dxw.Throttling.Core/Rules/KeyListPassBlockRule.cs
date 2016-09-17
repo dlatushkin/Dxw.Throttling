be947cece363bbc025d3670994c6e906f09103cc
dlatushkin <dxwizard@gmail.com>
2016-09-17 23:11:06 +0300

    start black/white lists

---
+++ Source/Projects/Dxw.Throttling.Core/Rules/KeyListPassBlockRule.cs
@@ -0,0 +1,46 @@
+﻿namespace Dxw.Throttling.Core.Rules
+{
+    using System;
+    using System.Threading.Tasks;
+    using System.Xml;
+    using System.Collections.Generic;
+
+    using Configuration;
+    using Keyers;
+    using Logging;
+
+    public class KeyListPassBlockRule<TArg> : IRule<TArg, PassBlockVerdict>
+    {
+        public IKeyer<TArg> Keyer { get; set; }
+
+        private ILog _log;
+
+        public string Name { get; private set; }
+
+        public IApplyResult<PassBlockVerdict> Apply(TArg context = default(TArg))
+        {
+            var key = Keyer.GetKey(context);
+
+            throw new NotImplementedException();
+            //return Has
+        }
+
+        public Task<IApplyResult<PassBlockVerdict>> ApplyAsync(TArg context = default(TArg))
+        {
+            throw new NotImplementedException();
+        }
+
+        //public override IApplyResult<PassBlockVerdict> Apply(TArg context = default(TArg));
+        //{
+        //    var key = Keyer.GetKey(context);
+
+        //    if (_blackList.Contains(key))
+        //    {
+
+        //    }
+        //}
+
+        //public abstract Task<IApplyResult<PassBlockVerdict>> ApplyAsync(TArg context = default(TArg));
+    }
+}
+
---
+++ Source/Projects/Dxw.Throttling.Core/Rules/KeyListRule.cs
@@ -0,0 +1,47 @@
+﻿namespace Dxw.Throttling.Core.Rules
+{
+    using System;
+    using System.Threading.Tasks;
+    using System.Xml;
+    using System.Collections.Generic;
+
+    using Configuration;
+    using Keyers;
+    using Logging;
+
+    public abstract class ListRule<TArg, TRes> : IRule<TArg, TRes>, IXmlConfigurable<TArg, TRes>
+    {
+        private List<object> _keys = new List<object>();
+
+        protected bool HasKey(object key)
+        {
+            return _keys.Contains(key);
+        }
+
+        public IKeyer<TArg> Keyer { get; set; }
+
+        private ILog _log;
+
+        public string Name { get; private set; }
+
+        public abstract IApplyResult<TRes> Apply(TArg context = default(TArg));
+
+        public abstract Task<IApplyResult<TRes>> ApplyAsync(TArg context = default(TArg));
+
+        public void Configure(XmlNode node, IConfiguration<TArg, TRes> context)
+        {
+            Name = node.Attributes["name"]?.Value;
+
+            _log = context.Log;
+            _log.Log(LogLevel.Debug, string.Format("Configuring rule '{0}' of type '{1}'", Name, GetType().FullName));
+
+            _keys.Clear();
+            var keySection = node.SelectSingleNode("keys");
+            foreach (XmlNode nKey in keySection)
+            {
+                var key = nKey.Value;
+                _keys.Add(key);
+            }
+        }
+    }
+}
